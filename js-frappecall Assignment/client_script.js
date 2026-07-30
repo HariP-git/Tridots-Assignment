ef1d1d34f44726d5973f69d69988e5672be047d3
HariP-git <prasath.official.2006@gmail.com>
2026-07-30 14:17:36 +0530

    firsrt

---
+++ js-frappecall Assignment/client_script.js
@@ -0,0 +1,38 @@
+let d = new frappe.ui.Dialog({
+    title: "Create Task",
+    fields: [
+        {
+            label: "Task Subject",
+            fieldname: "task_subject",
+            fieldtype: "Data",
+            reqd: 1
+        }
+    ],
+    primary_action_label: "Create Task",
+
+    primary_action(values) {
+
+        frappe.call({
+            method: "cafe_management.api.create_task",
+
+            args: {
+                task_subject: values.task_subject
+            },
+
+            callback: function(r) {
+
+                d.hide();
+
+                frappe.msgprint({
+                    title: "Success",
+                    message: "Task Created Successfully : " + r.message,
+                    indicator: "green"
+                });
+
+            }
+        });
+
+    }
+});
+
+d.show();
